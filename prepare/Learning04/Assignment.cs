ca5d90758d929fabcf72bd7a3c992498f99a0563
Skriller22 <matthewremington@mac.com>
2024-05-16 20:53:00 -0600

    Completed Week 04 Learning

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,26 @@
+public class Assignment
+{
+    private string _studentName = "John Doe";
+    private string _topic = "C# Programming";
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Matthew Remington", "C# Programming");
+
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Mandy Wilde", "Algebra", "2.2", "3-5");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("John Doe", "Narrative Writing", "My Summer Vacation");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
