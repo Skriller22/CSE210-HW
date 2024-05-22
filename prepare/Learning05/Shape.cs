6f3d528a76e3bb1e41455272317cf1a40a0679bc
Skriller22 <matthewremington@mac.com>
2024-05-22 16:45:43 -0600

    Basic Setup for assignment

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,19 @@
+public class Shape
+{
+    private string _color;
+
+    private string GetColor()
+    {
+        return _color;
+    }
+
+    private void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    private double GetArea()
+    {
+        return 0;
+    }
+}
